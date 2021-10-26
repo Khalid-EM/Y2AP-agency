5ba68fe86cac93e93d0281b4fba70697f7b35e2a
El Moussaoui Khalid (s108709) <khalid.elmoussaoui@student.ap.be>
2021-10-26 14:00:19 +0200

    Finished React in Gatsby

---
+++ src/components/greeting.js
@@ -0,0 +1,7 @@
+// Defineren van de <Greeting> component
+const Greeting = (props) => {
+    return (
+      <p>Hi {props.name}!</p>
+    )
+  }
+  
---
+++ src/pages/say-hello.js
@@ -0,0 +1,13 @@
+// Renderen van de <Greeting> component
+/*
+const SayHello = () => {
+    return (
+      <div>
+        <Greeting name="Megan" />
+        <Greeting name="Obinna" />
+        <Greeting name="Generosa" />
+      </div>
+    )
+  }
+  
+  export default SayHello*/
